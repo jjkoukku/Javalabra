57a8022df18c18591e89e7f26aacf7642caf6861
Joonas J Koukkunen <jjkoukku@dhp-25.cs.helsinki.fi>
2013-01-16 13:29:09 +0200

    projetkifilut päivitetty

---
+++ btHelsinki/src/bthelsinki/BtHelsinki.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bthelsinki;
+
+/**
+ *
+ * @author jjkoukku
+ */
+public class BtHelsinki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
